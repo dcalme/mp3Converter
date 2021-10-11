70e04676681e0a98cf7b5f6dd8592af91b1d3c40
dcalme <dariuscodoman@gmail.com>
2021-10-11 22:52:24 +0200

    add prompt lib

--- index.js
+++ index.js
@@ -1,51 +1,57 @@
 /* Modules */
 const puppeteer = require('puppeteer');
-const fetch = require('./lib.js');
+const prompt = require('prompt-sync')();
 const ora = require('ora');
+const fetch = require('./lib.js');
 
 /* Config */
 const config = require('./conf.json');
 
+// URL YouTube input
+const url = process.argv[2]
+
+// Get Artist & Song Title from user
+const artiste = prompt('Artist ? ');
+const songtitle = prompt('Title ? ');
 
 // Custom cli-progress
 const throbber = ora({spinner: config.spinner_type, text: config.spinner_text});
 throbber.start();
 
-// URL YouTube input
-const url = process.argv[2]
+crawl();
 
 
 async function crawl() {
+
     const browser = await puppeteer.launch({
       headless: true,
     });
     const page = await browser.newPage();
     
-    await page.goto(config.service_url);
-    await page.type(config.input_el, url);
-    await page.click(config.button_el);
+    await page.goto(config.service_url)
+    await page.type(config.input_el, url)
+    await page.click(config.button_el)
   
     page.on('response', async (response) => {    
         try { 
             const body = await response.json();
-
             if (body.data.fileUrl !== null) { // Get mp3 url from final response
-                let {title, fileUrl} = body.data;
-                await fetch.getMP3File(fileUrl, title);
-                throbber.succeed(config.spinner_succed);
-                console.log(title);
-                await browser.close();
+                let { fileUrl } = body.data
+                let title = `${artiste} - ${songtitle}`
+                await fetch.getMP3File(fileUrl, title, artiste, songtitle)
+                throbber.succeed(config.spinner_succed)
+                await browser.close()
             }
-
         } catch(error) {
             if (!error instanceof SyntaxError) { // JSON input body not existing
-                console.log(error);
+                console.log(error)
             }
         }
-    }); 
+    })
+    
 }
   
-crawl();
+
 
 
 
--- lib.js
+++ lib.js
@@ -1,18 +1,41 @@
 /* Modules */
 const fs = require('fs');
 const axios = require('axios');
+const { exec } = require("child_process");
 
 /* Config */
 const config = require('./conf.json');
 
-async function getMP3File(file_url, title) {
+async function getMP3File(file_url, title, artiste, songtitle) {
     axios.get(file_url, {responseType: "stream"})
     .then(function (response) {
-        response.data.pipe(fs.createWriteStream(`${config.path}${title}.mp3`));
+        let stream = fs.createWriteStream(`${config.path}${title}.mp3`)
+        response.data.pipe(stream)
+
+        stream.on('finish', () => {
+            console.log('File Created')
+            editMP3metadata(artiste, title, songtitle)
+        })
     })
     .catch(function (error) {
-        console.log(error);
-    });
+        console.log("Error during download")
+        console.log(error)
+    })
+}
+
+function editMP3metadata(artiste, title, songtitle) {
+    exec(`id3v2 -a "${artiste}" "${config.path}${title}.mp3" && id3v2 -t "${songtitle}" "${config.path}${title}.mp3"`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return
+        }
+        console.log(`stdout: ${stdout}`);
+        return
+    })
 }
 
 module.exports = {
